3bc891909021b55cb55265dfc81787d0ef3accd2
Taeguk Kwon <xornrbboy@gmail.com>
2016-09-24 23:53:30 +0900

    Implement Data Model Classes. Project #1 1.1 finish.

---
+++ FileProcessingProj/FileProcessingProj/model.cpp
@@ -0,0 +1,134 @@
+#include "model.h"
+
+
+namespace Model {
+
+
+	/*
+		Definitions of Member.
+	*/
+
+	Member::Member(
+		const std::string& id, const std::string& name,
+		const std::string& phone_number, const std::string& address,
+		const std::string& birthday, const std::string& email
+		)
+		: m_id(id), m_name(name), m_phone_number(phone_number),
+		m_address(address), m_birthday(birthday), m_email(email)
+	{
+	}
+
+	Member::Member(const Member& m)
+		: m_id(m.m_id), m_name(m.m_name), m_phone_number(m.m_phone_number),
+		m_address(m.m_address), m_birthday(m.m_birthday), m_email(m.m_email)
+	{
+	}
+
+	Member& Member::operator=(const Member& m)
+	{
+		this->m_id = m.m_id;
+		this->m_name = m.m_name;
+		this->m_phone_number = m.m_phone_number;
+		this->m_address = m.m_address;
+		this->m_birthday = m.m_birthday;
+		this->m_email = m.m_email;
+		return *this;
+	}
+
+	bool Member::operator==(const Member& m)
+	{
+		return this->m_id == m.m_id;
+	}
+
+	bool Member::operator!=(const Member& m)
+	{
+		return !(*this == m);
+	}
+
+	
+	/*
+		Definitions of Stock.
+	*/
+
+	Stock::Stock(
+		const std::string& id, const std::string& category,
+		const std::string& material, const std::string& price,
+		const std::string& stock, const std::string& washing_info,
+		const std::string& size
+		)
+		: m_id(id), m_category(category), m_material(material), m_price(price),
+		m_stock(stock), m_washing_info(washing_info), m_size(size)
+	{
+	}
+
+	Stock::Stock(const Stock& s)
+		: m_id(s.m_id), m_category(s.m_category), m_material(s.m_material), 
+		m_price(s.m_price), m_stock(s.m_stock), m_washing_info(s.m_washing_info), 
+		m_size(s.m_size)
+	{
+	}
+
+	Stock& Stock::operator=(const Stock& s)
+	{
+		this->m_id = s.m_id;
+		this->m_category = s.m_category;
+		this->m_material = s.m_material;
+		this->m_price = s.m_price;
+		this->m_stock = s.m_stock;
+		this->m_washing_info = s.m_washing_info;
+		this->m_size = s.m_size;
+
+		return *this;
+	}
+
+	bool Stock::operator==(const Stock& s)
+	{
+		return m_id == s.m_id;
+	}
+
+	bool Stock::operator!=(const Stock& s)
+	{
+		return !(*this == s);
+	}
+
+
+	/*
+		Definitions of Purchase.
+	*/
+
+	Purchase::Purchase(
+		const std::string& purchase_id, const std::string& stock_id, 
+		const std::string& member_id, const std::string& quantity
+		)
+		: m_purchase_id(purchase_id), m_stock_id(stock_id),
+		m_member_id(member_id), m_quantity(quantity)
+	{
+	}
+
+	Purchase::Purchase(const Purchase& p)
+		: m_purchase_id(p.m_purchase_id), m_stock_id(p.m_stock_id),
+		m_member_id(p.m_member_id), m_quantity(p.m_quantity)
+	{
+	}
+
+	Purchase& Purchase::operator=(const Purchase& p)
+	{
+		this->m_purchase_id = p.m_purchase_id;
+		this->m_stock_id = p.m_stock_id;
+		this->m_member_id = p.m_member_id;
+		this->m_quantity = p.m_quantity;
+
+		return *this;
+	}
+
+	bool Purchase::operator==(const Purchase& p)
+	{
+		return this->m_purchase_id == p.m_purchase_id;
+	}
+
+	bool Purchase::operator!=(const Purchase& p)
+	{
+		return !(*this == p);
+	}
+
+}
---
+++ FileProcessingProj/FileProcessingProj/model.h
@@ -0,0 +1,130 @@
+#include <string>
+
+
+/*
+	Data Model에 관련된 것들을 담고 있는 namespace Model
+*/
+namespace Model {
+
+
+	/*
+		각종 상수들
+	*/
+	const int MEMBER_BIRTHDAY_LEN = 8;
+	const int STOCK_ID_LEN = 12;
+	const int STOCK_CATEGORY_LEN = 8;
+	const int STOCK_PRICE_LEN = 6;
+	const int STOCK_STOCK_LEN = 4;
+	const int STOCK_SIZE_LEN = 2;
+	const int PURCHASE_ID_LEN = 16;
+	const int PURCHASE_QUANTITY_LEN = 2;
+
+
+	/*
+		회원 정보를 나타내는 클래스 Member
+	*/
+	class Member {
+	public:
+		Member(const std::string& id, const std::string& name, 
+			const std::string& phone_number, const std::string& address, 
+			const std::string& birthday, const std::string& email);
+		Member(const Member&);
+		Member& operator=(const Member&);
+		bool operator==(const Member&);
+		bool operator!=(const Member&);
+
+		inline std::string id();
+		inline std::string name();
+		inline std::string phone_number();
+		inline std::string address();
+		inline std::string birthday();
+		inline std::string email();
+
+	private:
+		std::string m_id;
+		std::string m_name;
+		std::string m_phone_number;
+		std::string m_address;
+		std::string m_birthday;
+		std::string m_email;
+	};
+
+	std::string Member::id() { return m_id; }
+	std::string Member::name() { return m_name; }
+	std::string Member::phone_number() { return m_phone_number; }
+	std::string Member::address() { return m_address; }
+	std::string Member::birthday() { return m_birthday; }
+	std::string Member::email() { return m_email; }
+
+
+	/*
+		재고 상품에 대한 정보를 나타내는 나타내는 Stock
+	*/
+	class Stock {
+	public:
+		Stock(const std::string& id, const std::string& category, 
+			const std::string& material, const std::string& price,
+			const std::string& stock, const std::string& washing_info,
+			const std::string& size);
+		Stock(const Stock&);
+		Stock& operator=(const Stock&);
+		bool operator==(const Stock&);
+		bool operator!=(const Stock&);
+
+		inline std::string id();
+		inline std::string category();
+		inline std::string material();
+		inline std::string price();
+		inline std::string stock();
+		inline std::string washing_info();
+		inline std::string size();
+
+	private:
+		std::string m_id;
+		std::string m_category;
+		std::string m_material;
+		std::string m_price;
+		std::string m_stock;
+		std::string m_washing_info;
+		std::string m_size;
+	};
+	
+	std::string Stock::id() { return m_id; }
+	std::string Stock::category() { return m_category; }
+	std::string Stock::material() { return m_material; }
+	std::string Stock::price() { return m_price; }
+	std::string Stock::stock() { return m_stock; }
+	std::string Stock::washing_info() { return m_washing_info; }
+	std::string Stock::size() { return m_size; }
+
+
+	/*
+		상품 구매 내역에 대한 정보를 나타내는 Purchase
+	*/
+	class Purchase {
+	public:
+		Purchase(const std::string& purchase_id, const std::string& stock_id,
+			const std::string& member_id, const std::string& quantity);
+		Purchase(const Purchase&);
+		Purchase& operator=(const Purchase&);
+		bool operator==(const Purchase&);
+		bool operator!=(const Purchase&);
+
+		inline std::string purchase_id();
+		inline std::string stock_id();
+		inline std::string member_id();
+		inline std::string quantity();
+
+	private:
+		std::string m_purchase_id;
+		std::string m_stock_id;
+		std::string m_member_id;
+		std::string m_quantity;
+	};
+
+	std::string Purchase::purchase_id() { return m_purchase_id; }
+	std::string Purchase::stock_id() { return m_stock_id; }
+	std::string Purchase::member_id() { return m_member_id; }
+	std::string Purchase::quantity() { return m_quantity; }
+
+}
